c6635621ff2a4e75f53a1958513d7dca379c07e5
kirillzykov <kirillzykov777@gmail.com>
2018-05-10 18:35:13 +0300

    BusStop and PublicTransport class added

---
+++ PublicTransportSimulator/BusStop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicTransportSimulator
+{
+    internal class BusStop
+    {
+        private int ID { get; set; }
+        private double weight { get; set; }
+        private string name { get; set; }
+        private List<int> adjacentIdList { get; set; }
+        private List<List<double>> adjacentRoadsList { get; set; }
+        private List<int> routeList { get; set; }
+
+        public BusStop()
+        {
+        }
+    }
+}
---
+++ PublicTransportSimulator/PublicTransport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicTransportSimulator
+{
+    internal class PublicTransport
+    {
+        private int routeId { get; set; }
+        private string transportId { get; set; }
+        private string transportType { get; set; }
+    }
+}
